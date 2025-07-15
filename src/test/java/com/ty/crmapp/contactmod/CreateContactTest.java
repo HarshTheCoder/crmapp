9027367330c616475914e77fa4bb39043be35a20
Harsh <Harsh@LAPTOP-UE5K2I7F>
2025-07-15 16:59:19 +0530

    first commmit

---
+++ src/test/java/com/ty/crmapp/contactmod/CreateContactTest.java
@@ -0,0 +1,17 @@
+package com.ty.crmapp.contactmod;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void createContact() {
+		System.out.println("Contact Created!!!!");
+	}
+	
+	@Test
+	public void createContactByEmail() {
+		System.out.println("Contact Created By Email!!!!");
+	}
+
+}
---
+++ src/test/java/com/ty/crmapp/orgmod/CreateOrgTest.java
@@ -0,0 +1,16 @@
+package com.ty.crmapp.orgmod;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrg() {
+		System.out.println("Organization Created!!!!");
+	}
+
+	@Test
+	public void createOrgByName() {
+		System.out.println("Organization Created By Name!!!!");
+	}
+}
